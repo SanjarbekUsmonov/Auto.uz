09c8213336bc5a07d4023d1c0a9020394e4c3cfa
SanjarbekUsmonov <noobpusk@gmail.com>
2022-12-06 02:26:08 +0500

    select

--- src/components/car-market/CompCarTobuy.vue
+++ src/components/car-market/CompCarTobuy.vue
@@ -3,23 +3,59 @@
       <div class="title text-h4 text-weight-bolder">
          Купить автомобиль
       </div>
-      <div class="carbuy shadow-10 q-mt-xl">
-
+      <div class="carbuy q-pa-md  q-col-gutter-xs row wrap justify-between  shadow-10 q-mt-xl">
+          <div class="select" style="width: 270px">
+            <q-select standout="bg-teal text-white" v-model="model" :options="options" label="Custom standout" />
+          </div>
+          <div class="select" style="width: 270px">
+            <q-select standout="bg-teal text-white" v-model="model" :options="options" label="Custom standout" />
+          </div>
+          <div class="select" style="width: 270px">
+            <q-select standout="bg-teal text-white" v-model="model" :options="options" label="Custom standout" />
+          </div>
+          <div class="select" style="width: 270px">
+            <q-select standout="bg-teal text-white" v-model="model" :options="options" label="Custom standout" />
+          </div>
+          <div class="select" style="width: 270px">
+            <q-select standout="bg-teal text-white" v-model="model" :options="options" label="Custom standout" />
+          </div>
+          <div class="select" style="width: 270px">
+            <q-select standout="bg-teal text-white" v-model="model" :options="options" label="Custom standout" />
+          </div>
+          <div class="select" style="width: 270px">
+            <q-select standout="bg-teal text-white" v-model="model" :options="options" label="Custom standout" />
+          </div>
+          <div class="select" style="width: 270px">
+            <q-select standout="bg-teal text-white" v-model="model" :options="options" label="Custom standout" />
+          </div>
+          <div class="select" style="width: 270px">
+            <q-select standout="bg-teal text-white" v-model="model" :options="options" label="Custom standout" />
+          </div>
       </div>
   </div>
 </template>
 
 <script>
-  export default {
+ import { ref } from 'vue'
 
+ export default {
+  setup () {
+    return {
+      model: ref(null),
+      options: [
+        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+      ]
+    }
   }
+}
 </script>
 
 <style scoped>
 .carbuy{
   width: 100%;
-  height: 250px;
+  height: auto;
   background: #fff;
   border-radius: 10px;
 }
+
 </style>
