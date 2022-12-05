85f549b9202dc597a0904edcf93c43f026297635
SanjarbekUsmonov <noobpusk@gmail.com>
2022-12-05 20:11:48 +0500

    components carbuy cartobuy

---
+++ src/components/car-market/CompCarTobuy.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="q-mt-lg">
+      <div class="title text-h4 text-weight-bolder">
+         Купить автомобиль
+      </div>
+      <div class="carbuy shadow-10 q-mt-xl">
+
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+.carbuy{
+  width: 100%;
+  height: 250px;
+  background: #fff;
+  border-radius: 10px;
+}
+</style>
--- src/pages/CarMarket.vue
+++ src/pages/CarMarket.vue
@@ -3,29 +3,30 @@
       <CompCarSimiller/>
       <div class="container">
           <div class="content">
-
+            <CompCarTobuy/>
+            <CompCarbuy/>
           </div>
-
       </div>
   </div>
 </template>
 
 <script>
 import CompCarSimiller from 'src/components/CompCarSimiller.vue';
+import CompCarTobuy from '../components/car-market/CompCarTobuy.vue';
+import CompCarbuy from '../components/car-market/CompCarbuy.vue'
 
   export default {
-    components: { CompCarSimiller }
+    components: { CompCarSimiller,CompCarTobuy,CompCarbuy }
 }
 </script>
 
 <style  scoped>
 .container{
-  height: 100vh;
+  width: 100%;
 }
 .content{
-  width: 75%;
-  height: 600px;
+  width: 70%;
+  height: auto;
   margin: 0 auto;
-  background: blue;
 }
 </style>
