84b3ed76eafb61d06ef477b00a0bf019deaa5a08
Alimardon9459 <mullajonovalimardon@gmail.com>
2022-12-19 15:29:09 +0500

    change

--- src/components/car-market/CarCard.vue
+++ src/components/car-market/CarCard.vue
@@ -1,109 +1,123 @@
 <template >
-  <div v-for="i in 5" :key="i">
-    <div class=" p-15px card-car">
-      <div class="w-30pr car__carusel">
-        <div class="car__title-tel"> 
-            <div class="w-100pr fs-16px">
-                <div class=" car__name">Infiniti Q30 , 2017</div>
-                <div class="row justify-between mb-10px">
-                    <div class="car__prince" >2 050 000 ₽</div>
-                    <div >40 000 KM</div>
-                </div>
-            </div>
-        </div>
-        <div>
-          <q-carousel
-            animated
-            v-model="slide"
-            arrows
-            infinite
-            height="200px"
-            class="br-7px"
-          >
-            <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg"
-            />
-            <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg"
-            />
-            <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg"
-            />
-          </q-carousel>
-        </div>
-      </div>
-      <div class="w-60pr row car__info pl-10px">
-        <div class="row w-100pr fs-16px car__title-komp">
-          <div class="w-33pr car__name">Infiniti Q30 , 2017</div>
-          <div class="w-33pr "><span class="car__prince">2 050 000 ₽</span></div>
-          <div class="w-33pr">40 000 KM</div>
-        </div>
-        <div class="w-33pr text-grey car__info-info">
-          <div>2.0 L /211 L.S/Benzin</div>
-          <div>Robot</div>
-          <div>Xetchbek 5 DV.</div>
-        </div>
-        <div class="w-33pr text-grey">
-          <div>Ploxoy</div>
-          <div>Beliy</div>
-        </div>
-        <div class="w-33pr car__verified ">
-          <div class="fs-16px text-green">
-            <q-icon name="verified" color="green" size="28px" /> Проверенный    дилер
+  <div>
+    <q-intersection
+      v-for="index in 60"
+      :key="index"
+      transition="scale"
+      class="example-item"      
+      >       
+      
+      <div class=" p-15px card-car">
+        <div class="w-30pr car__carusel">
+          <div class="car__title-tel"> 
+              <div class="w-100pr fs-16px">
+                  <div class=" car__name">Infiniti Q30 , 2017</div>
+                  <div class="row justify-between mb-10px">
+                      <div class="car__prince" >2 050 000 ₽</div>
+                      <div >40 000 KM</div>
+                  </div>
+              </div>
           </div>
-          <div class="text-blue-7 mt-5px">U bir narsa +</div>
-        </div>
-        <div class="w-100pr">
           <div>
-            <span class="p-5px bg-grey-3 text-blue-7 br-5px fs-12px">
-              Maximum - avotaojsk goda
-            </span>
+            <q-carousel
+              animated
+              v-model="slide"
+              arrows
+              infinite
+              height="200px"
+              class="br-7px"
+            >
+              <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg"
+              />
+              <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg"
+              />
+              <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+              />
+            </q-carousel>
           </div>
+        </div>
+        <div class="w-60pr row car__info pl-10px">
+          <div class="row w-100pr fs-16px car__title-komp">
+            <div class="w-33pr car__name">Infiniti Q30 , 2017</div>
+            <div class="w-33pr "><span class="car__prince">2 050 000 ₽</span></div>
+            <div class="w-33pr">40 000 KM</div>
+          </div>
+          <div class="w-33pr text-grey car__info-info">
+            <div>2.0 L /211 L.S/Benzin</div>
+            <div>Robot</div>
+            <div>Xetchbek 5 DV.</div>
+          </div>
+          <div class="w-33pr text-grey">
+            <div>Ploxoy</div>
+            <div>Beliy</div>
+          </div>
+          <div class="w-33pr car__verified ">
+            <div class="fs-16px text-green">
+              <q-icon name="verified" color="green" size="28px" /> Проверенный    дилер
+            </div>
+            <div class="text-blue-7 mt-5px">U bir narsa +</div>
+          </div>
+          <div class="w-100pr">
+            <div>
+              <span class="p-5px bg-grey-3 text-blue-7 br-5px fs-12px">
+                Maximum - avotaojsk goda
+              </span>
+            </div>
 
-          <div class=" mt-10px car__owner">
-            <div class="m-5px row justify-between items-center">
-                <div class="w-50pr">
-                    Mullajonov  Alimardon
-                </div>
-                <div>
-                    <q-btn class="bg-green text-white" padding="5px 10px"> Qo'ng'iroq </q-btn>
-                </div>
+            <div class=" mt-10px car__owner">
+              <div class="m-5px row justify-between items-center">
+                  <div class="w-50pr">
+                      Mullajonov  Alimardon
+                  </div>
+                  <div>
+                      <q-btn class="bg-green text-white" padding="5px 10px"> Qo'ng'iroq </q-btn>
+                  </div>
+              </div>
+            </div>
+
+            <div class="mt-10px row justify-between">
+              <div>
+                  <q-icon name="verified" color="green" size="22px" class=" comp " />Fergana
+              </div>
+              <div>
+                  <q-icon name="favorite_border"  size="22px" class=" comp " />
+              </div>
             </div>
           </div>
-
-          <div class="mt-10px row justify-between">
-            <div>
-                <q-icon name="verified" color="green" size="22px" class=" comp " /> Fergana
-            </div>
-            <div>
-                <q-icon name="favorite_border"  size="22px" class=" comp " />
-            </div>
+        </div>
+        <div class="w-30px car__icons-chect"></div>
+        <div class="w-30px h-120px br-5px car__icons" style="border: 1px solid grey">
+          <div class="row justify-center h-25pr items-center" style="border-bottom: 1px solid grey" >
+            <q-icon name="favorite_border" size="22px" />
+          </div>
+          <div class="row justify-center h-25pr items-center" style="border-bottom: 1px solid grey" >
+            <q-icon name="favorite_border" size="22px" />
+          </div>
+          <div class="row justify-center h-25pr items-center" style="border-bottom: 1px solid grey" >
+            <q-icon name="favorite_border" size="22px" />
+          </div>
+          <div class="row justify-center h-25pr items-center">
+            <q-icon name="favorite_border" size="22px" />
           </div>
         </div>
       </div>
-      <div class="w-30px car__icons-chect"></div>
-      <div class="w-30px h-120px br-5px car__icons" style="border: 1px solid grey">
-        <div class="row justify-center h-25pr items-center" style="border-bottom: 1px solid grey" >
-          <q-icon name="favorite_border" size="22px" />
-        </div>
-        <div class="row justify-center h-25pr items-center" style="border-bottom: 1px solid grey" >
-          <q-icon name="favorite_border" size="22px" />
-        </div>
-        <div class="row justify-center h-25pr items-center" style="border-bottom: 1px solid grey" >
-          <q-icon name="favorite_border" size="22px" />
-        </div>
-        <div class="row justify-center h-25pr items-center">
-          <q-icon name="favorite_border" size="22px" />
-        </div>
-      </div>
-    </div>
-   
+    </q-intersection>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import axios from 'axios';
 const slide = ref(1);
+
+
 </script>
 
 <style lang="css" scoped>
+.example-item{
+  width: 100%;
+  height: 250px;
+}
 .card-car{
     display: flex;
     justify-content: space-between;
@@ -143,6 +157,11 @@ const slide = ref(1);
     display: none;
 }
 @media screen and (max-width:600px) {
+    .example-item{
+        width: 100%;
+        height: 520px;
+         
+    }
     .card-car:hover {
         box-shadow: none ;
         cursor: pointer;
---
+++ src/pages/inter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="q-pa-md">
+    <div class="row justify-center q-gutter-sm">
+      <q-intersection
+        v-for="index in 60"
+        :key="index"
+        transition="scale"
+        class="example-item"
+      >
+        <CarCard/>
+      </q-intersection>
+    </div>
+  </div>
+</template>
+<script setup>
+import CarCard from 'src/components/car-market/CarCard.vue';
+
+
+</script>
+<style scoped>
+.example-item{
+  width: 100%;
+  height: 250px;
+}
+@media screen and (max-width:600px) {
+.example-item{
+    width: 100%;
+    height: 520px;
+  }   
+}
+  
+</style>
